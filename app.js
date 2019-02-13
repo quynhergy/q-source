e4faf11eb57015ae8766ff5da8f713493739212a
Quynh Nguyen <qnguyen@twitter.com>
2019-02-13 10:45:37 -0800

    Second commit

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/", function(req,res){
 	res.render("index");
 });
 
-app.listen(8000, process.env.IP,function(){
+app.listen(process.env.PORT, process.env.IP,function(){
 	console.log("SERVER IS RUNNING");
 });
