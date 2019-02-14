43f7c0c825cce15f54490bf0461b4129755d4c89
Quynh Nguyen <qnguyen@twitter.com>
2019-02-14 09:34:57 -0800

    Add social media page

--- app.js
+++ app.js
@@ -6,15 +6,19 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 app.get("/", function(req,res){
-	res.render('index');
+	res.render("index");
 });
 
 app.get("/under-construction", function(req,res){
-	res.render('under-construction');
+	res.render("under-construction");
 });
 
 app.get("/about-me", function(req,res){
-	res.render('about-me');
+	res.render("about-me");
+});
+
+app.get("/social-media", function(req,res){
+	res.render("social-media");
 });
 
 var port = process.env.PORT || 3000;
