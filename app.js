35355c92b0f07259fd27256e906ccc7b75b77418
Quynh Nguyen <qnguyen@twitter.com>
2019-02-13 21:53:37 -0800

    Made div clickable and add under construction page

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.get("/", function(req,res){
 	res.render('index');
 });
 
+app.get("/under-construction", function(req,res){
+	res.render('under-construction');
+})
+
 var port = process.env.PORT || 3000;
 
 var server = app.listen(port, process.env.IP,function(){
